3157c62e86af9c8a4e808a57e311ad3b377bc18f
Jeremy Retailleau <jeremy.retailleau@gmail.com>
2019-09-08 18:31:01 -0700

    Update docstrings

--- source/stylish/filesystem.py
+++ source/stylish/filesystem.py
@@ -12,38 +12,65 @@ import skimage.transform
 import stylish.logging
 
 
-def load_image(image_path, image_size=None):
+def load_image(path, image_size=None):
     """Return 3-D Numpy array from image *path*.
 
-    *image_size* can be specified to resize the image.
+    :param path: path to image file to load.
+
+    :param image_size: targeted size of the image matrix loaded. By default, the
+        image will not be resized.
+
+    :return: 3-D Numpy array representing the image loaded.
+
+    .. note::
+
+        `Lists of all formats currently supported
+        <https://imageio.readthedocs.io/en/stable/formats.html>`_
 
     """
     logger = stylish.logging.Logger(__name__ + ".load_image")
-    logger.debug("Load image from path: {!r}".format(image_path))
+    logger.debug("Load image from path: {!r}".format(path))
 
-    matrix = imageio.imread(image_path).astype(np.float)
+    image = imageio.imread(path).astype(np.float)
 
     # If the image is monochrome, make it into an RGB image.
-    if not (len(matrix.shape) == 3 and matrix.shape[2] == 3):
-        matrix = np.dstack((matrix, matrix, matrix))
+    if not (len(image.shape) == 3 and image.shape[2] == 3):
+        image = np.dstack((image, image, image))
 
     if image_size is not None:
-        matrix = skimage.transform.resize(matrix, image_size)
+        image = skimage.transform.resize(image, image_size)
 
-    return matrix
+    return image
 
 
-def save_image(image_matrix, path):
-    """Save *image_matrix* to *path*."""
-    image = np.clip(image_matrix, 0, 255).astype(np.uint8)
+def save_image(image, path):
+    """Save *image_matrix* to *path*.
+
+    :param image: 3-D Numpy array representing the image to save.
+
+    :param path: path to image file to save *image* into.
+
+    :return: None
+
+    .. note::
+
+        `Lists of all formats currently supported
+        <https://imageio.readthedocs.io/en/stable/formats.html>`_
+
+    """
+    image = np.clip(image, 0, 255).astype(np.uint8)
     imageio.imwrite(path, image)
 
 
 def fetch_images(path, limit=None):
     """Return list of image paths from *path*.
 
-    *limit* should be the maximum number of files to fetch from *path*. By
+    :param path: path to the directory containing all images to fetch.
+
+    :param limit: maximum number of files to fetch from *path*. By
     default, all files are fetched.
+
+    :return: list of image file path.
 
     """
     if not os.path.isdir(path) or not os.access(path, os.R_OK):
@@ -58,7 +85,13 @@ def fetch_images(path, limit=None):
 
 
 def ensure_directory(path):
-    """Ensure directory exists at *path*."""
+    """Ensure directory exists at *path*.
+
+    :param path: directory path.
+
+    :return: None
+
+    """
     # Explicitly indicate that path should be a directory as default OSError
     # raised by 'os.makedirs' just indicates that the file exists, which is a
     # bit confusing for user.
@@ -78,10 +111,16 @@ def ensure_directory(path):
 def sanitise_value(value, substitution_character="_", case_sensitive=True):
     """Return *value* suitable for use with filesystem.
 
-    Replace awkward characters with *substitution_character*. Where possible,
-    convert unicode characters to their closest "normal" form.
+    :param value: string value to sanitise.
 
-    If not *case_sensitive*, then also lowercase value.
+    :param substitution_character: string character to replace awkward
+        characters with. Default is "_".
+
+    :param case_sensitive: indicate whether sanitised value should be kept with
+        original case. Otherwise, the sanitised value will be return in
+        lowercase. By default, the original case is preserved.
+
+    :return: sanitised value.
 
     """
     value = unicodedata.normalize("NFKD", value)
